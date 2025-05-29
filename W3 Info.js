1400d462f46602af243b72f369ab1e77aa85d648
2JuanRodriguez3 <juanito82006@gmail.com>
2025-05-29 01:20:26 -0700

    Add files via upload

---
+++ W3 Info.js
@@ -0,0 +1,17 @@
+            // Single Line Comment
+            /*
+                Multi Line Comment
+            */
+            var firstname = "Juan"; //sometimes you give a varible it's starting value
+            let lastname;  // sometimes you don't know th varible's value but you still need to create it.
+            // This is a good javascript syntax to create but, this is an example and isn't the proper way.
+            // When varibles are made like this, it's no clear as to where the varible first comes from.
+            helmet="protection";
+
+            // JavaScript is loosly typed out, so javascript guesses what the datatype is for your variables.
+            // adding numbers can cause issues becasue then JavaScript thinks that the numbers are strings.
+
+            // Add the h2 text using JavaScript
+            document.getElementById("h2subtitle").textContent = "JavaScript Here We Come.";
+            // Add the paragraph text
+             document.getElementById("pMoreInfo").textContent = "JavaScript is typed out very loose, which makes it dynamicaly.";
